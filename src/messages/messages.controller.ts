397ff6f2ee943129671623ae73f854ef53216d0a
sujitha <sujitha.agamworks@gmail.com>
2024-07-31 06:23:09 +0530

    learned where to use body and param decorators and how to use restclient in vs code

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post,Body, Param,Query,Header } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,15 +8,16 @@ export class MessagesController {
     
     }
     @Post()
-    createMessage()
+    createMessage(@Body() body:any)
     {
         console.log("they created a new mesage");
+        console.log(body);
     }
 
     @Get('/:id')
-    getParticularMessage()
+    getParticularMessage(@Param('id') id:String)
     {
-
+console.log(id);
     }
 
 }
